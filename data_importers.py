def254acead1a0e60a3a8bcf9b58fb515a95ddfa
Bryan Nova <bryanherman7@gmail.com>
2025-03-08 17:00:02 -0500

    switch to pre-processed FACED

--- data_importers.py
+++ data_importers.py
@@ -1,4 +1,4 @@
-import mne
+import pickle
 import glob
 import os
 import re
@@ -8,20 +8,22 @@ def import_FACED(data_directory):
     """
     FACED: Finer-grained Affective Computing EEG Dataset
     https://www.synapse.org/Synapse:syn50614821
+    Use the Processed_data.zip file
     """
     eeg_data = []
-    print('Reading FACED data')
+    print('Reading FACED data ...')
     # cohorts 1 and 2 have different placement for EEG channels; just take cohort 1
     valid_subj_ids_re = r'sub0([0-5][0-9]|60)'
     subjects_to_consider = [
-        f for f in glob.glob(os.path.join(data_directory, "**/data.bdf"), recursive=True) if _filepath_regex(f, valid_subj_ids_re)
+        f for f in glob.glob(os.path.join(data_directory, "**/sub*.pkl"), recursive=True) if _filepath_regex(f, valid_subj_ids_re)
     ]
     for file in subjects_to_consider:
-        try:
-            raw_data = mne.io.read_raw_bdf(file, preload=True)
-            eeg_data.append(raw_data)
-        except e as Exception:
-            raise e
+        print(file)
+        with open(file, "rb") as data:
+            try:
+                eeg_data.append(pickle.load(data))
+            except e as Exception:
+                raise e
     input_data = np.stack(eeg_data, axis=0)
     return input_data
 
