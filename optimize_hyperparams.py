8a42e1fbcf89dfadea1719fe592c230a0b6d6bb5
Bryan Nova <bryanherman7@gmail.com>
2025-04-14 15:35:54 -0400

    update hyperparam optimization

--- optimize_hyperparams.py
+++ optimize_hyperparams.py
@@ -1,41 +1,63 @@
 import numpy as np
 import math
-from training import train_adam, qae_cost_function, qte_cost_function, ENTANGLEMENT_OPTIONS, ENTANGLEMENT_GATES
+
+from training import *
+from loss import *
+from analysis import check_for_overfitting, MODEL_TYPES
 
 MAX_NUM_BLOCKS = 1 # per encoder AND per decoder
 
-def sample_hyperparameters(num_qubits):
+def sample_hyperparameters(num_features):
     return {
-        'bottleneck_size': 2,
+        'bottleneck_size': num_features // 2,
         'num_blocks': np.random.randint(1, MAX_NUM_BLOCKS+1), # per encoder AND per decoder
-        'learning_rate': 10 ** np.random.uniform(-4, -1),
-        'penalty_weight': .75,
+        'learning_rate': 10 ** np.random.uniform(-3, -1),
+        'max_penalty_weight': 2.0,
         'entanglement_topology': np.random.choice(ENTANGLEMENT_OPTIONS),
         'entanglement_gate': np.random.choice(ENTANGLEMENT_GATES),
     }
 
-def get_loss(data, type, config, allocated_epochs):
+def get_loss(data, model_type, config, allocated_epochs):
     print(config)
     np.random.seed(12984)
-    if type.lower() == 'qae':
-        trained_params, cost_history, validation_cost, _, _, _ = \
-            train_adam(data[0], data[1], qae_cost_function, config, allocated_epochs)
-    elif type.lower() == 'qte':
-        trained_params, cost_history, validation_cost, _, _, _ = \
-            train_adam(data[0], data[1], qte_cost_function, config, allocated_epochs)
+
+    training = data[0]
+    validation = data[1]
+    num_features = len(training[0][1][0])
+
+    print('Training ' + model_type.upper())
+    is_recurrent = 'r' in model_type
+    autoregressive = 'te' in model_type # transition encoder
+
+    if model_type.startswith('c'):
+        model = ClassicalEncoderDecoder(num_features, config, is_recurrent)
+        trash_penalty_fn = classical_trash_penalty
+    elif model_type.startswith('q'):
+        model = QuantumEncoderDecoder(num_features, config, is_recurrent)
+        trash_penalty_fn = trash_qubit_penalty
     else:
-        raise Exception('Unknown type: ' + type)
-    print(' ', validation_cost)
+        raise Exception('Unexpected model type: ' + model_type)
+
+    if autoregressive:
+        loss_fn = autoencoder_cost_function(trash_penalty_fn)
+    else:
+        loss_fn = autoregressive_cost_function(trash_penalty_fn)
+
+    trained_model, cost_history, validation_costs = \
+        train_adam(training, validation, loss_fn, config, model, allocated_epochs)
+
+    print(' ', validation_costs)
     if allocated_epochs >= 10:
-        overfit_cost = abs(validation_cost - cost_history[-1]) / max(validation_cost, cost_history[-1])
-        if overfit_cost > 1./3.: # throw away any configs that lead to obvious overfitting
+        mean_training_costs = cost_history[-1] / len(training)
+        mean_validation_costs = np.sum(validation_costs[:,1:], axis=0) / len(validation)
+        if check_for_overfitting(mean_training_costs, mean_validation_costs): # throw away any configs that lead to obvious overfitting
             return float('inf')
     # scale the cost by the % of max num of blocks used
     # TODO: incorporate bottleneck size
     scale = float(config['num_blocks']) / float(MAX_NUM_BLOCKS)
-    return cost_history[-1] * scale
+    return sum(cost_history[-1]) * scale
 
-def hyperband_search(data, type, max_training_epochs=100, reduction_factor=3):
+def hyperband_search(data, max_training_epochs=16, reduction_factor=2):
     """
     Parameters:
       - type: "QAE" or "QTE" (case-insensitive)
@@ -45,21 +67,21 @@ def hyperband_search(data, type, max_training_epochs=100, reduction_factor=3):
     Returns:
       optimal_config, optimal_loss
     """
-    print(f'Performing hyperband search over {type.upper()} training hyperparameters')
+    print(f'Performing hyperband search over training hyperparameters')
     max_bracket = int(np.floor(np.log(max_training_epochs) / np.log(reduction_factor)))
     total_budget = (max_bracket + 1) * max_training_epochs
     optimal_config = None
     optimal_loss = float('inf')
 
-    print('num series in training:', len(data[0]))
-    num_qubits = len(data[0][0])
+    print('  num series in training:', len(data[0]))
+    num_features = len(data[0][0][1][0])
 
     for bracket in reversed(range(max_bracket + 1)):
         initial_num_configs = int(np.ceil(total_budget / max_training_epochs / (bracket + 1) * reduction_factor ** bracket))
         initial_allocated_epochs = max_training_epochs * reduction_factor ** (-bracket)
 
         print(f"Bracket {max_bracket-bracket}: Starting with {initial_num_configs} configurations, each with {initial_allocated_epochs} epochs.")
-        configs = [sample_hyperparameters(num_qubits) for _ in range(initial_num_configs)]
+        configs = [sample_hyperparameters(num_features) for _ in range(initial_num_configs)]
         print('Created configs')
 
         # successive reduction in num configs
@@ -68,7 +90,7 @@ def hyperband_search(data, type, max_training_epochs=100, reduction_factor=3):
             epochs_this_round = int(initial_allocated_epochs * reduction_factor ** (round_index))
 
             print('Round', round_index, '-', num_configs_this_round, 'configs for', epochs_this_round, ' epochs this round')
-            round_losses = [get_loss(data, type, config, epochs_this_round) for config in configs]
+            round_losses = [np.mean([get_loss(data, m, config, epochs_this_round) for m in MODEL_TYPES]) for config in configs]
             print(f"  Round {round_index}: {epochs_this_round} epochs; best loss = {min(round_losses):.4f}")
 
             for i, loss in enumerate(round_losses):
@@ -87,9 +109,8 @@ if __name__ == '__main__':
         description="Optimize the hyperparameters of the QAE or QTE training for this experiment."
     )
     parser.add_argument("data_directory", type=str, help="Path to the directory containing the training data.")
-    parser.add_argument("--type", type=str, default='qte', help="QAE or QTE (case-insensitive)")
-    parser.add_argument("--reduction_factor", type=int, default=3, help="Factor by which successive configuration evals are reduced each round.")
-    parser.add_argument("--max_training_epochs", type=int, default=27, help="Maximum number of epochs allocated to any configuration.")
+    parser.add_argument("--reduction_factor", type=int, default=2, help="Factor by which successive configuration evals are reduced each round.")
+    parser.add_argument("--max_training_epochs", type=int, default=16, help="Maximum number of epochs allocated to any configuration.")
     args = parser.parse_args()
 
     from data_importers import import_generated
@@ -102,7 +123,7 @@ if __name__ == '__main__':
         v = np.random.randint(0, len(validation))
         input_data[1].append(validation[v])
 
-    best_config, best_loss = hyperband_search(input_data, args.type, args.max_training_epochs, args.reduction_factor)
+    best_config, best_loss = hyperband_search(input_data, args.max_training_epochs, args.reduction_factor)
     print("\nBest hyperparameter configuration found:")
     print(best_config)
     print("With estimated loss:", best_loss)
