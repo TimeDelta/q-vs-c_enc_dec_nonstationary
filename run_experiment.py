4b49415958248dbbaf83a6ee071d8674690bd67f
Bryan Nova <bryanherman7@gmail.com>
2025-04-15 12:15:20 -0400

    fixes

--- run_experiment.py
+++ run_experiment.py
@@ -11,17 +11,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Run this experiment."
     )
-    parser.add_argument("--data_directory", type=str, default='generated_datasets', help="Directory in which to store the generated data.")
+    parser.add_argument("data_directory", type=str, nargs='?', default='generated_datasets', help="Directory in which to store the generated data.")
     args = parser.parse_args()
 
     if not os.path.exists(args.data_directory):
         generate_data(args.data_directory)
+    else:
+        print('!! Skipping data generation due to existing specified directory')
     dataset_partitions = import_generated(args.data_directory)
 
     best_config_path = os.path.join(args.data_directory, 'best_config.json')
     if os.path.exists(best_config_path):
         with open(best_config_path, 'r') as file:
             best_config = json.load(file)
+        print('Loaded best config from hyperparameter optimization')
     else:
         best_config = get_best_config(dataset_partitions)
         with open(best_config_path, 'w') as file:
@@ -30,7 +33,11 @@ if __name__ == '__main__':
     num_features = len(next(iter(dataset_partitions.values()))[0][0][1][0])
     pytorch_models = len(glob.glob(os.path.join(args.data_directory, '*.pth')))
     qiskit_models = len(glob.glob(os.path.join(args.data_directory, '*.qpy')))
-    if pytorch_models + qiskit_models == len(dataset_partitions) * len(MODEL_TYPES):
+    num_required_models = len(dataset_partitions) * len(MODEL_TYPES)
+    num_missing_models = num_required_models - (pytorch_models + qiskit_models)
+    if num_missing_models > 0:
+        if pytorch_models > 0 or qiskit_models > 0:
+            print(f'Missing {num_missing_models} models')
         train_and_analyze_bottlenecks(args.data_directory, dataset_partitions, num_features, num_epochs, best_config)
     run_analysis(dataset_partitions, args.data_directory)
     print('Experiment complete')
